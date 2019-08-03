cd164d651ba76edaa9ec0a1afcfcfa184d6c71c3
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-08-03 03:57:03 +0200

    finished script for detecting symmetry in image

--- scripts/nicolas_scripts/old/detect_sym_simple.py
+++ scripts/nicolas_scripts/old/detect_sym_simple.py
@@ -1,9 +1,11 @@
 
 import sys, os
 import numpy as np
-from scipy import misc
+import imageio
 import copy
 import sct_utils as sct
+from msct_register import find_angle_hog
+from nicolas_scripts.functions_sym_rot import generate_2Dimage_line
 
 
 def main(args=None):
@@ -16,26 +18,23 @@ def main(args=None):
 
     for image_fname in list_image_fname:
 
-        image = misc.imread(image_directory + "/" + image_fname).astype(int)
+        image = imageio.imread(image_directory + "/" + image_fname)
 
         if len(image.shape) > 2:
             image = np.mean(image[:, :, 0:2], axis=2)
 
         image_axes = np.zeros((image.shape[0], image.shape[1], 3))
-        image_axes[:, :, 0] = copy.copy(image)
         image_axes[:, :, 1] = copy.copy(image)
         image_axes[:, :, 2] = copy.copy(image)
 
-        seg_image = (image > np.mean(np.concatenate(image))).astype(int)
+        seg_image = (image > np.mean(np.concatenate(image)))
+        # TODO do seg of the image
 
-        angle_hog, conf_score, centermass = find_angle(image, seg_image, 0.0001, 0.0001, "hog", angle_range=90, return_centermass=True, save_figure_path=image_directory + "/fig_sym_" + image_fname)
-        if angle_hog is None:
-            angle_hog = 0
-        angle_pca, _, centermass = find_angle(image, seg_image, 1, 1, "pca", angle_range=90, return_centermass=True)
-        image_axes[:, :, 1] = generate_2Dimage_line(image, centermass[0], centermass[1], 2*pi - angle_hog + pi/2)
-        image_axes[:, :, 2] = generate_2Dimage_line(image, centermass[0], centermass[1], 2*pi - angle_pca)
+        angle, conf_score = find_angle_hog(image, (image.shape[0]//2, image.shape[1]//2), 0.0001, 0.0001, angle_range=90)
+        image_axes[:, :, 0] = generate_2Dimage_line(copy.copy(image), image.shape[0]//2, image.shape[1]//2, angle)
 
-        misc.imsave(image_directory + "/sym_" + image_fname.split(".")[0] + ".png", image_axes, "png")
+        imageio.imsave(image_directory + "/sym_" + image_fname.split(".")[0] + ".png", image_axes, "png")
+
 
 if __name__ == '__main__':
 
